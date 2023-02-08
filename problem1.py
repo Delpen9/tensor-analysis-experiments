7524d44c68113dce55b2b4c244d8fadd388e75e1
ian.dover <ian.dover@skunova.com>
2023-02-08 01:03:58 +0000

    finished part a and b of problem 1

--- problem1.py
+++ problem1.py
@@ -26,6 +26,29 @@ if __name__ == '__main__':
         [0.7430, 0.8040]
     ])
 
+    # Part A
     outer_product_u11_u21 = np.outer(a = _u1[:, 0], b = _u2[:, 0])
 
-    print(outer_product_u11_u21)
+    print(fr'''
+Here is the calculation for the first outer product:
+{outer_product_u11_u21}
+    ''')
+
+    # Part B
+    # The outer product is associative
+    rank_1_kruskal = np.outer(a = _u1[:, 0], b = _u2[:, 0])
+    rank_1_kruskal = np.outer(a = rank_1_kruskal, b = _u3[:, 0])
+    rank_1_kruskal = np.outer(a = rank_1_kruskal, b = _u4[:, 0])
+    rank_1_kruskal *= _lambda[0]
+
+    rank_2_kruskal = np.outer(a = _u1[:, 1], b = _u2[:, 1])
+    rank_2_kruskal = np.outer(a = rank_2_kruskal, b = _u3[:, 1])
+    rank_2_kruskal = np.outer(a = rank_2_kruskal, b = _u4[:, 1])
+    rank_2_kruskal *= _lambda[1]
+
+    kruskal_tensor = rank_1_kruskal + rank_2_kruskal
+    print(fr'''
+Here is the calculation for the Kruskal tensor:
+{kruskal_tensor}
+    ''')
+
