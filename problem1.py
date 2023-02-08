dfb8eff58e4cf8766c168c58aab49c5fb5107794
ian.dover <ian.dover@skunova.com>
2023-02-08 01:44:56 +0000

    performed tucker decomposition and recomposition

--- problem1.py
+++ problem1.py
@@ -1,4 +1,21 @@
 import numpy as np
+from tensorly import tucker_to_tensor
+from tensorly.decomposition import tucker
+
+def tucker_reconstruction(G, U_modes):
+    num_modes = len(G.shape)
+    mode_dims = [U_mode.shape[0] for U_mode in U_modes]
+
+    reconstructed_tensor = np.zeros(mode_dims)
+    for mode_n in range(num_modes):
+        mode_matrix = U_modes[mode_n]
+        mode_matrix_t = np.transpose(mode_matrix)
+        for i, row in enumerate(mode_matrix_t):
+            index = [slice(None) for _ in range(num_modes)]
+            index[mode_n] = i
+            reconstructed_tensor[tuple(index)] = np.tensordot(row, G, axes = (1, mode_n))
+
+    return reconstructed_tensor
 
 if __name__ == '__main__':
     _lambda = np.array([41.9075, 18.6722])
@@ -62,3 +79,69 @@ Here is the calculation for the Kruskal tensor:
 {kruskal_tensor}
     ''')
 
+    # Part 2
+    _g_11 = np.array([
+        [35.2489, 0.7832],
+        [0.2884, -4.2162]
+    ])
+
+    _g_21 = np.array([
+        [0.3406,   2.0238],
+        [-1.6475, -2.4122]
+    ])
+
+    _g_12 = np.array([
+        [0.0883, -1.4479],
+        [0.9827,  3.4852]
+    ])
+
+    _g_22 = np.array([
+        [1.0571, -3.3204],
+        [4.3714, -7.8907]
+    ])
+
+    core = np.array([
+        [_g_11, _g_12],
+        [_g_21, _g_22]
+    ])
+
+    _u1 = np.array([
+        [0.6276, -0.0110],
+        [0.5511, -0.7000],
+        [0.5499, -0.7141]
+    ])
+
+    _u2 = np.array([
+        [0.5840,  0.6977],
+        [0.3660,  0.2300],
+        [0.7246, -0.6785]
+    ])
+
+    _u3 = np.array([
+        [0.6157, -0.4874],
+        [0.5428,  0.8319],
+        [0.5712, -0.2651]
+    ])
+
+    _u4 = np.array([
+        [0.6827,  0.7307],
+        [0.7307, -0.6827]
+    ])
+
+    tucker_tensor = tucker_to_tensor(
+        tucker_tensor = (
+            core,
+            [_u1, _u2, _u3, _u4]
+        )
+    )
+
+    print(fr'''
+Here is the calculation for the Tucker tensor:
+{tucker_tensor}
+    ''')
+
+    # Validate the Tucker recomposition
+    core, factors = tucker(
+        tucker_tensor,
+        rank = [2, 2, 2, 2]
+    )
