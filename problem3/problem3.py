5979f80ccb8146ff20542d1fbd7be884f35785be
ian.dover <ian.dover@skunova.com>
2023-02-11 21:02:24 +0000

    made additional changes to problem 3

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def tucker_decomposition_als(
     tensor : np.ndarray,
-    R1 : int,
-    R2 : int,
-    R3 : int,
-    max_iterations : int = 100,
+    R1 : int = 30,
+    R2 : int = 30,
+    R3 : int = 20,
+    max_iterations : int = 50,
     tolerance : float = 1e-4
 ) -> tuple[
         np.ndarray,
@@ -19,15 +19,15 @@ def tucker_decomposition_als(
     tensor : numpy.ndarray
         The input tensor with shape (I, J, K).
     R1 : int
-        The desired rank of the first factor matrix.
+        The desired rank of the first factor matrix, by default 30.
     R2 : int
-        The desired rank of the second factor matrix.
+        The desired rank of the second factor matrix, by default 30.
     R3 : int
-        The desired rank of the third factor matrix.
+        The desired rank of the third factor matrix, by default 20.
     max_iterations : int, optional
-        The maximum number of iterations to perform, by default 100.
+        The maximum number of iterations to perform, by default 50.
     tolerance : float, optional
-        The tolerance for the residual, by default 1e-5.
+        The tolerance for the residual, by default 1e-4.
 
     Returns
     -------
@@ -37,23 +37,59 @@ def tucker_decomposition_als(
         Factor matrices
     '''
     I, J, K = tensor.shape
+
     A = np.random.rand(I, R1)
     B = np.random.rand(J, R2)
     C = np.random.rand(K, R3)
+
     for iteration in range(max_iterations):
         for i in range(I):
             A[i, :] = np.linalg.lstsq(np.dot(B, C.T), tensor[i, :, :].flatten(), rcond=None)[0]
+
         for j in range(J):
             B[j, :] = np.linalg.lstsq(np.dot(A, C.T), tensor[:, j, :].flatten(), rcond=None)[0]
+
         for k in range(K):
             C[k, :] = np.linalg.lstsq(np.dot(A, B.T), tensor[:, :, k].flatten(), rcond=None)[0]
+
         reconstructed_tensor = np.einsum('ijk,ik,jk->ijj', tensor, A, B, C)
+
         residual = np.linalg.norm(tensor - reconstructed_tensor)
         if residual < tolerance:
             break
+
     core_tensor = np.einsum('ik,jk,kk->ijk', A, B, C)
     return (core_tensor, (A, B, C))
 
-if __name__ == '__main__':
+def relative_error(
+    tensor : np.ndarray,
+    core : np.ndarray
+) -> float:
+    '''
+    Calculate the relative error between a tensor and its core tensor.
 
+    Parameters
+    ----------
+    tensor : numpy.ndarray
+        The original tensor.
+    core : numpy.ndarray
+        The core tensor obtained from decomposition.
+
+    Returns
+    -------
+    float
+        The relative error between the original tensor and its core tensor.
+
+    '''
+    tensor_frobenius_norm = np.linalg.norm(tensor, 'fro')
+    core_frobenius_norm = np.linalg.norm(core, 'fro')
+
+    numerator = np.sqrt(tensor_frobenius_norm**2 - core_frobenius_norm**2)
+    denominator = tensor_frobenius_norm
+
+    relative_error = numerator / denominator
+
+    return relative_error
+
+if __name__ == '__main__':
     core, factors = tucker_decomposition_als()
