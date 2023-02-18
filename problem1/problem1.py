59913eb4156f95f3d46f0168665eb49fe29f0ef1
ian.dover <ian.dover@skunova.com>
2023-02-18 23:44:14 +0000

    fixed problem 1

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     file_directory = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'Problem1'))
 
     _text_1, _text_2, _text_3, _text_4 = part1()
-    _text_4 = part2()
+    _text_5 = part2()
     _text_6, _text_7 = part3()
 
-    text_list = [_text_1, _text_2, _text_3, _text_4, _text_4, _text_6, _text_7]
+    text_list = [_text_1, _text_2, _text_3, _text_4, _text_5, _text_6, _text_7]
     for i in range(len(text_list)):
         with open(fr'{file_directory}/problem1_{i + 1}.txt', 'w') as filewriter:
             filewriter.write(text_list[i])
